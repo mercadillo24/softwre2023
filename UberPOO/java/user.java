6cfcff8615ab7d6ff2ac23b45219357dcd9c8d55
brayan <brayanmercadillo24@gmail.com>
2023-05-02 16:27:38 -0600

    tercer commit

--- UberPOO/java/user.java
+++ UberPOO/java/user.java
@@ -1,4 +1,15 @@
 public class user extends Account {
     public User(String name, string 
-    document, string)
+    document, string email, String
+    password){
+        super(name, document, email,
+        password);
+    }
+    void printDataUser(){
+        System.out.println("Document
+        driver: " + document + " name
+        drver: " + name + " E-mail: "
+        + email + password: "+
+        password");
+    }
 }
